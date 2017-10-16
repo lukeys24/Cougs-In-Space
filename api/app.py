ec9fc322a04b9496cbd40bd87552773604e16503
Jared <Jared@eecs.wsu.edu>
2017-10-15 23:37:42 -0700

    Completely rehauled app.py to conform to our websites basic functionality standards, will implement and update more functionality as neccesary. Must decide if user credentials checking should be done on client or server side.

--- api/app.py
+++ api/app.py
@@ -11,24 +11,29 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlalchemy-demo.db'
 db = sqlalchemy.SQLAlchemy(app)
 base_url = '/api/'
 
+#Team post template, will allow logged in user to dynamically add or delete a content post stored in a database
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	team = db.Column(db.String(128))
 	title = db.Column(db.String(64))
 	update = db.Column(db.String(2048))
+	like_count = db.Column(db.Integer)
 	created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
+#User class to store and manage access to adding and deleting content buttons
 class User(db.Model):
 	name = db.Column(db.String(50), primary_key=True)
 	password = db.Column(db.String(50))
 
 # index
-# loads all smiles given a space, count parameter and order_by parameter 
-# if the count param is specified and doesn't equal all limit by the count
-# if the order_by param is specified order by param otherwise load by updated_at desc
-# return JSON
-@app.route(base_url + 'posts', methods=["GET"])
-def index():
+#Loads all the given team posts that are currently hidden
+#when javascript event is triggered to allow a set of posts to come into view call show team,
+#those posts will be shown
+#called when a certain team is being viewed to show top 5 recent team posts
+#loads all posts given a team, count parameter(5 default) and order_by parameter(newest first default) 
+#return JSON
+@app.route(base_url + 'posts/<String:team>', methods=["GET"])
+def index(team):
     count = request.args.get('count', None)
     order_by = request.args.get('order_by', None)
     team = request.args.get('team', None) 
@@ -48,62 +53,44 @@ def index():
 
 
 # show
-# loads a smile given the id as a value in the URL
-# TODO 4: create the route for show
-@app.route(base_url + 'smiles/<int:id>', methods=["POST"])
+# loads team post given the id as a value in the URL
+@app.route(base_url + 'posts/<int:id>', methods=["POST"])
 def show(id):
-	row = Smile.query.filter_by(id=id).first()
-	return jsonify({"smile": row_to_obj(row), "status": 1}), 200
+	row = Post.query.filter_by(id=id).first()
+	return jsonify({"post": row_to_obj(row), "status": 1}), 200
 
 
 # create
-# creates a smile given the params
-# TODO 5: create the route for create
-@app.route(base_url + 'smiles', methods=['POST'])
-def create():
-    smile = Smile(**request.json)
-    db.session.add(smile)
+# creates a team post given the params
+@app.route(base_url + 'posts/<String:team>', methods=['POST'])
+def create(team):
+    post = Post(**request.json)
+    db.session.add(post)
     db.session.commit()
 
-    db.session.refresh(smile)
+    db.session.refresh(post)
 
-    return jsonify({"status": 1, "smile": row_to_obj(smile)}), 200
+    return jsonify({"status": 1, "post": row_to_obj(post)}), 200
 
-# delete_smiles
-# delete given an space
-# delete all smiles in that space
-# TODO 6: create the route for delete_smiles
-@app.route(base_url + 'smiles', methods=['DELETE'])
-def delete_smiles():
-	space = request.args.get('space', None)
+# delete_entire_post_history
+# delete entire given team db history
+@app.route(base_url + 'posts/<String:team>', methods=['DELETE'])
+def delete_entire_post_history():
+	team = request.args.get('team', None)
 	
-	if (space is None):
+	if (team is None ):
 		return jsonify({"Error": -1}), 500
 	else:
-		Smile.query.filter_by(space=space).delete()
+		Post.query.filter_by(Post=post).delete()  #needs to be (team=team)?
 		db.session.commit()
 		return jsonify({"status": 1}), 200
-	
-	
-	#Smile.filter_by(space=space).delete()
-	
-	#db.session.query(space).delete().all()
-	#db.session.delete(smile)
-	#db.session.commit()
-	#db.session.refresh(smile)
 
 # post_like
-# loads a smile given an ID and increments the count by 1
-# TODO 7: create the route for post_like UNFINISHED      ASk SHARLDSFIIRRRAAAA
-@app.route(base_url + 'smiles/<int:id>/like', methods=['POST'])
+#loads a post by its clicked ID and increments its like value
+@app.route(base_url + 'posts/<int:id>/like', methods=['POST'])
 def post_like(id):
-	#row = Smile.query.filter_by(id=id).first()
-	#row.attr.like_count = row.like_count + 1
-	#db.session.add(row)
-	#db.session.commit()
-	#db.session.refresh(row)
 	
-	Smile.query.filter_by(id=id).update({Smile.like_count: Smile.like_count + 1})
+	Post.query.filter_by(id=id).update({Post.like_count: Post.like_count + 1})
 	db.session.commit()
 	return jsonify({"status Updated": 1}), 200
 	
@@ -113,6 +100,7 @@ def row_to_obj(row):
             "team": row.team,
             "title": row.title,
             "update": row.update,
+            "like_count": row.like_count,
             "created_at": row.created_at,
         }
 
