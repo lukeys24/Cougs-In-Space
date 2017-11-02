991bf0241121a8cbb7bf247ef078b42e108b828c
lseo <lseo@eecs.wsu.edu>
2017-11-01 20:49:27 -0700

    Fixed onclick functionality

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,28 +1,8 @@
 var Cougs_In_Space = (function() {
 
 
-    var attachHomePageHandler = function(e) {
-        // First, hide the forms and page content initially
-		index.find('home-page').show();      //HOMEPAGE HANDLER
-		index.find('team-page').hide();
-		index.find('donors-page').hide();
-		index.find('get-involved-page').hide();
-		index.find('donate-page').hide();
-		index.find('K-12-page').hide();
-		index.find('about-page').hide();
-		index.find('systems-page').hide();
-		index.find('power-page').hide();
-		index.find('attitude-page').hide();
-		index.find('structure-page').hide();
-		index.find('thermal-page"').hide();
-
-    };
-
-
-
     var start = function() {
 
-        attachHomePageHandler();
     };
 
     // PUBLIC METHODS
@@ -35,65 +15,66 @@ var Cougs_In_Space = (function() {
 })();
 
 function showPage(id) {
-    alert(id);
+    cis = $(".cis");
     if (id == 'Home-navbar') {
-
+         cis.find('.home-page').show();
+         alert("test");
     } else {
-
+         cis.find('.home-page').hide();
     }
     if (id == 'Our-Teams') {
-
+        cis.find('.team-page').show();
     } else {
-        
+        cis.find('.team-page').hide();
     }
     if (id == 'Systems-dropdown') {
-
+        cis.find('.systems-page').show();
     } else {
-        
+        cis.find('.systems-page').hide();
     }
     if (id == 'Power-dropdown') {
-
+        cis.find('.power-page').show();
     } else {
-        
+        cis.find('.power-page').hide();
     }
     if (id == 'Attitude-dropdown') {
-
+        cis.find('.attitude-page').show();
     } else {
-        
+        cis.find('.attitude-page').hide();
     }
     if (id == 'Structures-dropdown') {
-
+        cis.find('.structure-page').show();
     } else {
-        
+        cis.find('.structure-page').hide();
     }
     if (id == 'Thermal-dropdown') {
-
+        cis.find('.thermal-page').show();
     } else {
-        
+        cis.find('.thermal-page').hide();
     }
     if (id == 'Donors-navbar') {
-
+        cis.find('.donors-page').show();
     } else {
-        
+        cis.find('.donors-page').hide();   
     }
     if (id == 'GetInvolved-navbar') {
-
+        cis.find('.get-involved-page').show();
     } else {
-        
+        cis.find('.get-involved-page').hide();
     }
     if (id == 'Donate-navbar') {
-
+        cis.find('.donate-page').show();
     } else {
-        
+        cis.find('.donate-page').hide();
     }
     if (id == 'K-12-navbar') {
-
+        cis.find('.k-12-page').show();
     } else {
-        
+        cis.find('.k-12-page').hide();
     }
     if (id == 'About-navbar') {
-
+        cis.find('.about-page').show();
     } else {
-        
+        cis.find('.about-page').hide();
     }
 }
