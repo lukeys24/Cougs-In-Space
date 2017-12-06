6fcb22f8f5548c0cd3fd25f13e660bff46a93865
Jared <jared@eecs.wsu.edu>
2017-12-05 20:43:11 -0800

    pausing to checkout master

--- api/app.py
+++ api/app.py
@@ -13,11 +13,9 @@ db = sqlalchemy.SQLAlchemy(app)
 #Team post template, will allow logged in user to dynamically add or delete a content post stored in a database
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	team = db.Column(db.String(128))
+	team = db.Column(db.String(64))
 	title = db.Column(db.String(64))
 	update = db.Column(db.String(2048))
-	like_count = db.Column(db.Integer)
-	created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
 #User class to store and manage access to adding and deleting content buttons
 class User(db.Model):
@@ -34,15 +32,12 @@ base_url = '/api/'
 #loads all posts given a team, count parameter(5 default) and order_by parameter(newest first default) 
 #return JSON
 @app.route(base_url + 'posts/<string:team>', methods=["GET"])
-def index(team):
-    count = request.args.get('count', None)
-    order_by = request.args.get('order_by', None)
-    team = request.args.get('team', None) 
+def index():
 
     if team is None:
         return "Must provide team", 500
     
-    query = Post.query.filter_by(team=team).order_by(order_by).limit(count).all() # store the results of your query here 
+    query = Post.query.filter_by(team=team).order_by(id).limit(count).all() # store the results of your query here 
     
     result = []
     for row in query:
@@ -53,31 +48,31 @@ def index(team):
     return jsonify({"status": 1, "posts": result})
 
 
-@app.route(base_url + 'login/<string:username>/', methods=["GET"])
-def index(team):
-    count = request.args.get('count', None)
-    order_by = request.args.get('order_by', None)
-    team = request.args.get('team', None) 
+# @app.route(base_url + 'login/<string:username>', methods=["GET"])
+# def index(team):
+#     count = request.args.get('count', None)
+#     order_by = request.args.get('order_by', None)
+#     team = request.args.get('team', None) 
 
-    if team is None:
-        return "Must provide team", 500
+#     if team is None:
+#         return "Must provide team", 500
     
-    query = Post.query.filter_by(team=team).order_by(order_by).limit(count).all() # store the results of your query here 
+#     query = Post.query.filter_by(team=team).order_by(order_by).limit(count).all() # store the results of your query here 
     
-    result = []
-    for row in query:
-        result.append(
-            row_to_obj(row) # you must call this function to properly format 
-        )
+#     result = []
+#     for row in query:
+#         result.append(
+#             row_to_obj(row) # you must call this function to properly format 
+#         )
 
-    return jsonify({"status": 1, "posts": result})
+#     return jsonify({"status": 1, "posts": result})
 
 # show
 # loads team post given the id as a value in the URL
-@app.route(base_url + 'posts/<string:team>/<int:id>', methods=["POST"])
-def show(id):
-	row = Post.query.filter_by(id=id).first()
-	return jsonify({"post": row_to_obj(row), "status": 1}), 200
+# @app.route(base_url + 'posts/<string:team>/<int:id>', methods=["POST"])
+# def show(id):
+# 	row = Post.query.filter_by(id=id).first()
+# 	return jsonify({"post": row_to_obj(row), "status": 1}), 200
 
 
 # create
@@ -94,34 +89,31 @@ def create(team):
 
 # delete_entire_post_history
 # delete entire given team db history
-@app.route(base_url + 'posts/<int:team>', methods=['DELETE'])
-def delete_entire_post_history():
-	team = request.args.get('team', None)
+# @app.route(base_url + 'posts/<string:team>', methods=['DELETE'])
+# def delete_entire_post_history():
+# 	team = request.args.get('team', None)
 	
-	if (team is None ):
-		return jsonify({"Error": -1}), 500
-	else:
-		Post.query.filter_by(Post=post).delete()  #needs to be (team=team)?
-		db.session.commit()
-		return jsonify({"status": 1}), 200
+# 	if (team is None ):
+# 		return jsonify({"Error": -1}), 500
+# 	else:
+# 		Post.query.filter_by(Post=post).delete()  #needs to be (team=team)?
+# 		db.session.commit()
+# 		return jsonify({"status": 1}), 200
 
 # post_like
 #loads a post by its clicked ID and increments its like value
-@app.route(base_url + 'posts/<int:team>/<int:id>/like', methods=['POST'])
-def post_like(id):
+# @app.route(base_url + 'posts/<string:team>', methods=['POST'])
+# def post_post(id):
 	
-	Post.query.filter_by(id=id).update({Post.like_count: Post.like_count + 1})
-	db.session.commit()
-	return jsonify({"status Updated": 1}), 200
+# 	Post.query.filter_by(id=id).update({Post.like_count: Post.like_count + 1})
+# 	db.session.commit()
+# 	return jsonify({"status Updated": 1}), 200
 	
 def row_to_obj(row):
     row = {
             "id": row.id,
-            "team": row.team,
             "title": row.title,
-            "update": row.update,
-            "like_count": row.like_count,
-            "created_at": row.created_at,
+            "update": row.update
         }
 
     return row
